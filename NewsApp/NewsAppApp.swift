69809a646125c9c98d8d4d8864718807a9451e06
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2024-02-24 11:12:18 +0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Rahmonali on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
